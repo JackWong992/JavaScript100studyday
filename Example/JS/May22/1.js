025aac61b1a75e15d39d01570473b34c877b4942
JackWong992 <JackWong992@163.com>
2019-05-23 09:11:22 +0800

    finish may23 lesson 4

--- Example/JS/May22/1.js
+++ Example/JS/May22/1.js
@@ -12,3 +12,12 @@ var obj1 = {
 var obj2 = obj1
 obj2['n'] = 200;
 console.log(obj1.n) //200
+
+var obj = {
+  n: 10,
+  m: obj.n*10
+}
+
+console.log(obj.m)
+ //Error: Can not read property 'm' of undefiend
+
